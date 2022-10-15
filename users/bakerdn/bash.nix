f74daad4b5fa81277c335ab72feffed39aa514b7
Daniel Baker <daniel.n.baker@gmail.com>
2022-10-15 05:59:55 -0700

    added lock command.

--- users/bakerdn/bash.nix
+++ users/bakerdn/bash.nix
@@ -14,6 +14,22 @@
   programs.bash.shellAliases = {
     lse = "ls -Fho";
     lsa = "lse -A";
+    lock = ''
+      swaylock \
+      --screenshots \
+      --clock \
+      --indicator-radius 100 \
+      --indicator-thickness 7 \
+      --effect-blur 7x5 \
+      --effect-vignette 0.5:0.5 \
+      --ring-color bb00cc \
+      --key-hl-color 880033 \
+      --line-color 00000000 \
+      --inside-color 00000088 \
+      --separator-color 00000000 \
+      --grace 2 \
+      --fade-in 0.2
+    '';
   };
   programs.bash.shellOptions = [
     # Append to history file rather than replacing it.
